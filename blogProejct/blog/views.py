28e17ca1c6928efed5451445d8c09aa993ce1ae8
kianGs4 <kian.ghassemi1383@gmail.com>
2024-07-22 16:04:07 +0330

    added template with dummy data

--- blogProejct/blog/views.py
+++ blogProejct/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(renquest): 
-    return HttpResponse('<h1>Blog home<h1>')
+posts = [ 
+    {
+        'author': 'CoreyMs', 
+        'title' : 'nmd post', 
+        'content': "first post i have", 
+        'data_posted': 'june 21, 2024'
+    }, 
+    {
+        'author': 'mia', 
+        'title' : 'nmd post', 
+        'content': "second post i have", 
+        'data_posted': 'june 25, 2024'
+    }
+]
+
+def home(request): 
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request): 
-    return HttpResponse('<h1>Blog About<h1>')
+    return render(request, 'blog/about.html', {'title': 'about'} )
