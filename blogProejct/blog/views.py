14f34ba9353fd87db61fec6e44db10b232c89726
kianGs4 <kian.ghassemi1383@gmail.com>
2024-07-23 20:49:08 +0330

    added Post model

--- blogProejct/blog/views.py
+++ blogProejct/blog/views.py
@@ -1,24 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
-posts = [ 
-    {
-        'author': 'CoreyMs', 
-        'title' : 'nmd post', 
-        'content': "first post i have", 
-        'data_posted': 'june 21, 2024'
-    }, 
-    {
-        'author': 'mia', 
-        'title' : 'nmd post', 
-        'content': "second post i have", 
-        'data_posted': 'june 25, 2024'
-    }
-]
+
 
 def home(request): 
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
