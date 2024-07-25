a6996ca58fd27ac1913b9708aaa87a055ad8a0da
kianGs4 <kian.ghassemi1383@gmail.com>
2024-07-25 23:31:35 +0330

    email configuration and reset password added

--- blogProejct/blogProejct/settings.py
+++ blogProejct/blogProejct/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,4 +135,17 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
+# EMAIL_HOST_USER = "*****.**@gmail.com"
+# EMAIL_HOST_PASSWORD = "password"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 # LOGOUT_REDIRECT_URL = 'logout'
